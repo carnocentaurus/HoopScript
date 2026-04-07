d42c524c62b620dc606f513b0c345e59a87c781a
EJay <carnocentaurus@gmail.com>
2026-04-07 10:26:05 +0800

    project initialized

---
+++ src/data/teams.ts
@@ -0,0 +1,8 @@
+export const TEAMS = [
+  "Atlanta", "Boston", "Brooklyn", "Charlotte", "Chicago", 
+  "Cleveland", "Dallas", "Denver", "Detroit", "Houston", 
+  "Indiana", "Los Angeles", "Memphis", "Miami", "Milwaukee", 
+  "Minnesota", "New Orleans", "New York", "Oklahoma City", "Orlando", 
+  "Philadelphia", "Phoenix", "Portland", "Sacramento", "San Antonio", 
+  "San Diego", "San Francisco", "Toronto", "Utah", "Washington"
+].sort(); // This ensures alphabetical order automatically
