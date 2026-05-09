84064559f8cd1f7c5b305ff4ff0eb9f4d3a3e3bd
EJay <carnocentaurus@gmail.com>
2026-05-09 12:07:53 +0800

    app variants

---
+++ app.config.js
@@ -0,0 +1,54 @@
+const IS_DEV = process.env.APP_VARIANT === 'development';
+const ICON_PATH = IS_DEV 
+  ? './assets/images/icon/dev_icon.jpg' 
+  : './assets/images/icon/hoopscript_icon_resized.png';
+
+module.exports = {
+  expo: {
+    name: IS_DEV ? 'Dev' : 'HoopScript',
+    slug: 'HoopScript',
+    version: '1.0.1',
+    orientation: 'portrait',
+    icon: ICON_PATH,
+    jsEngine: 'hermes',
+    userInterfaceStyle: 'light',
+    splash: {
+      image: './assets/images/icon/hoopscript_icon_resized.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff'
+    },
+    ios: {
+      supportsTablet: true,
+      jsEngine: 'hermes',
+      bundleIdentifier: IS_DEV 
+        ? 'com.carnocentaurus.hoopscript.dev' 
+        : 'com.carnocentaurus.hoopscript'
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: ICON_PATH,
+        backgroundColor: IS_DEV ? '#00FFFF' : '#B34726'
+      },
+      jsEngine: 'hermes',
+      softwareKeyboardLayoutMode: 'pan',
+      versionCode: 2,
+      predictiveBackGestureEnabled: false,
+      permissions: [],
+      package: IS_DEV 
+        ? 'com.carnocentaurus.hoopscript.dev' 
+        : 'com.carnocentaurus.hoopscript'
+    },
+    web: {
+      favicon: './assets/images/icon/hoopscript_icon_resized.png'
+    },
+    plugins: [
+      'expo-font',
+      'expo-audio'
+    ],
+    extra: {
+      eas: {
+        projectId: 'f36aaf0d-7c26-4db4-b079-0225ae1c25d4'
+      }
+    }
+  }
+};
